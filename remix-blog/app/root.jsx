9bc3970c121b4a8cdb7c4ae959c6a55b4afcc1d3
Jake Scudder <scudsterr@gmail.com>
2022-01-10 20:50:06 -0500

    Following Tutorial
    Basic Functionality built out

--- remix-blog/app/root.jsx
+++ remix-blog/app/root.jsx
@@ -1,18 +1,37 @@
-import { Outlet, LiveReload } from "remix";
+import { Outlet, LiveReload, Link, Links, Meta } from "remix";
+import globalStylesUrl from "~/styles/global.css";
 
 export default function App() {
   return (
     <Document>
-      <Outlet />
+      <Layout>
+        <Outlet />
+      </Layout>
     </Document>
   );
 }
+
+export const links = () => [{ rel: "stylesheet", href: globalStylesUrl }];
+
+export const meta = () => {
+  const description = "Cool blog build with remix";
+  const keywords = "remix, react, javascript";
+
+  return {
+    description,
+    keywords,
+  };
+};
 
 function Document({ children, title }) {
   return (
     <html lang="en">
       My App
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
         <title>My Remix Blog</title>
       </head>
       <body>
@@ -20,5 +39,25 @@ function Document({ children, title }) {
         {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
       </body>
     </html>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          Remix
+        </Link>
+
+        <ul className="nav">
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div>{children}</div>
+    </>
   );
 }
